795964a686fbe7eea29a55d62b8d7289cc56251e
Oliver Berg <oliver@berg.moe>
2022-08-15 16:36:35 +0200

    Remove remal-gradle plugins
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,6 @@
-import name.remal.gradle_plugins.dsl.extensions.convention
-import name.remal.gradle_plugins.dsl.extensions.get
-import name.remal.gradle_plugins.plugins.publish.ossrh.RepositoryHandlerOssrhExtension
-
 plugins {
     kotlin("jvm").version("1.5.10")
     kotlin("kapt").version("1.5.10")
-    id("name.remal.maven-publish-ossrh") version "1.3.1"
-    id("name.remal.check-dependency-updates") version "1.3.1"
-    `maven-publish`
 }
 
 group = "dev.epubby"
@@ -73,44 +66,10 @@ tasks {
     compileKotlin {
         kotlinOptions {
             jvmTarget = "11"
-            freeCompilerArgs = freeCompilerArgs  + listOf(
+            freeCompilerArgs = freeCompilerArgs + listOf(
                 "-Xuse-experimental=kotlin.Experimental",
                 "-Xjvm-default=all"
             )
         }
     }
-}
-
-project.afterEvaluate {
-    publishing.publications.withType<MavenPublication> {
-        pom {
-            name.set(project.name)
-            description.set(project.description)
-            url.set("https://github.com/Olivki/epubby")
-
-            licenses {
-                license {
-                    name.set("The Apache Software License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    distribution.set("repo")
-                }
-            }
-
-            developers {
-                developer {
-                    id.set("Olivki")
-                    name.set("Oliver Berg")
-                    email.set("oliver@berg.moe")
-                }
-            }
-
-            scm {
-                connection.set("scm:git:git://github.com/Olivki/epubby")
-                developerConnection.set("scm:git:ssh://github.com/Olivki/epubby")
-                url.set("https://github.com/Olivki/epubby")
-            }
-        }
-    }
-
-    publishing.repositories.convention[RepositoryHandlerOssrhExtension::class.java].ossrh()
 }
