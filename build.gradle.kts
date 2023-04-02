94ad072a1571d97a05a66270e7d03175c40a5211
Oliver Berg <oliver@berg.moe>
2023-04-02 14:22:37 +0200

    Update dependencies
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,25 +16,25 @@ dependencies {
     implementation(kotlin("reflect"))
 
     // XML
-    implementation("org.jdom:jdom2:2.0.6")
+    implementation("org.jdom:jdom2:2.0.6.1")
 
     // Apache
-    implementation("commons-validator:commons-validator:1.6")
+    implementation("commons-validator:commons-validator:1.7")
     implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("commons-io:commons-io:2.7")
+    implementation("commons-io:commons-io:2.11.0")
 
     // logging
-    implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger:1.0.2")
+    implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger:1.0.4")
     implementation("io.arrow-kt:arrow-core:1.1.2")
 
     // css handler
-    api("com.helger:ph-css:6.2.0")
+    api("com.helger:ph-css:7.0.0")
 
     // JSoup
-    api("org.jsoup:jsoup:1.12.1")
+    api("org.jsoup:jsoup:1.15.4")
 
     // Google
-    api("com.google.guava:guava:28.1-jre")
+    api("com.google.guava:guava:31.1-jre")
 }
 
 tasks {
